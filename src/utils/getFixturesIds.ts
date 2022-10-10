ad9009aafb4828c5977723c963289d400cd9def5
KayckSchuenck <ycferreiras@gmail.com>
2022-10-10 17:58:13 -0300

    Done!

--- src/utils/getFixturesIds.ts
+++ src/utils/getFixturesIds.ts
@@ -1,6 +1,6 @@
 export async function getFixturesIds(hashtable:any){
     let string=''
-    Object.keys(hashtable).forEach(id=>{
+    Object.keys(hashtable).slice(0,19).forEach(id=>{
         string+=`${id}-`
     })
     return string.slice(0, -1)
