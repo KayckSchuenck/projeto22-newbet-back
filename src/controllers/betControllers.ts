680831532e2df8858f68e83b8eeb842f9192f3e4
KayckSchuenck <ycferreiras@gmail.com>
2022-10-11 01:54:34 -0300

    Fix Bug

--- src/controllers/betControllers.ts
+++ src/controllers/betControllers.ts
@@ -5,7 +5,7 @@ export async function createBetOptions(req:Request, res:Response){
     const {userId}=res.locals.id
     const {fixtureId,amount,odd,value}=req.body
     const availableMoney=await serviceCreateBetOptions(fixtureId,Number(amount),userId,Number(odd),value)
-    res.status(201).send(availableMoney)
+    res.status(201).send({availableMoney})
 }
 
 export async function createBetGoals(req:Request, res:Response){
@@ -13,14 +13,14 @@ export async function createBetGoals(req:Request, res:Response){
     const {userId}=res.locals.id
     const {fixtureId,amount,odd,value,type}=req.body
     const availableMoney=await serviceCreateBetGoals(fixtureId,Number(amount),userId,Number(odd),value,type,goalOrCorner)
-    res.status(201).send(availableMoney)
+    res.status(201).send({availableMoney})
 }
 
 export async function createBetScores(req:Request, res:Response){
     const {userId}=res.locals.id
     const {fixtureId,amount,odd,scoreHome,scoreAway}=req.body
     const availableMoney=await serviceCreateBetScores(fixtureId,Number(amount),userId,Number(odd),scoreHome,scoreAway)
-    res.status(201).send(availableMoney)
+    res.status(201).send({availableMoney})
 }
 
 export async function checkBets(req:Request, res:Response) {
